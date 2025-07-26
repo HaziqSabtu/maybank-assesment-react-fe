472be97840a1006d39d608983977e564b06b2bb3
haziqsabtu <mhaziq.sabtu@gmail.com>
2025-07-26 08:57:21 +0800

    feat: add zod

--- types/PlaceApi.ts
+++ types/PlaceApi.ts
@@ -1,25 +1,36 @@
-export type PlacePredictionResponse = {
-    suggestions: {
-        placePrediction: PlacePrediction;
-    }[];
-};
+import { z } from "zod";
 
-export type PlacePrediction = {
-    place: string;
-    placeId: string;
-    text: MatchedText;
-    structuredFormat: {
-        mainText: MatchedText;
-        secondaryText: {
-            text: string;
-        };
-    };
-    types: string[];
-};
+export const MatchedTextSchema = z.object({
+    text: z.string(),
+    matches: z.array(
+        z.object({
+            endOffset: z.number(),
+        })
+    ),
+});
 
-export type MatchedText = {
-    text: string;
-    matches: {
-        endOffset: number;
-    }[];
-};
+export const PlacePredictionSchema = z.object({
+    place: z.string(),
+    placeId: z.string(),
+    text: MatchedTextSchema,
+    structuredFormat: z.object({
+        mainText: MatchedTextSchema,
+        secondaryText: z.object({
+            text: z.string(),
+        }),
+    }),
+    types: z.array(z.string()),
+});
+
+export const PlacePredictionResponseSchema = z.object({
+    suggestions: z.array(
+        z.object({
+            placePrediction: PlacePredictionSchema,
+        })
+    ),
+});
+
+export type PlacePrediction = z.infer<typeof PlacePredictionSchema>;
+export type PlacePredictionResponse = z.infer<
+    typeof PlacePredictionResponseSchema
+>;
