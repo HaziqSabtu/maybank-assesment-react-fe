7e1f35658ae8b8dd9e8a2f4d094f8e9547d11984
haziqsabtu <mhaziq.sabtu@gmail.com>
2025-07-26 11:06:57 +0800

    feat: add caching place details response

--- features/place/placeDetailsSlice.ts
+++ features/place/placeDetailsSlice.ts
@@ -7,30 +7,47 @@ import {
 } from "@/types/PlaceApi";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const fetchPlaceById = createAsyncThunk<Place, string>(
-    "place/fetchById",
-    async (id: string, thunkAPI) => {
-        const json = await getPlaceDetails(id);
-        const parseResult = placeDetailsResponseSchema.safeParse(json);
-        if (!parseResult.success) {
-            console.error(parseResult.error);
-            return thunkAPI.rejectWithValue("Invalid response format");
-        }
+const CACHE_DURATION_MS = 1000 * 60 * 5;
 
-        return mapperToPlace(parseResult.data);
+export const fetchPlaceById = createAsyncThunk<
+    Place,
+    string,
+    { state: RootState }
+>("place/fetchById", async (id: string, thunkAPI) => {
+    const cached = thunkAPI.getState().placeDetails.cache[id];
+    const now = Date.now();
+
+    if (cached && now - cached.timestamp < CACHE_DURATION_MS) {
+        return cached.data;
     }
-);
+
+    const json = await getPlaceDetails(id);
+    const parseResult = placeDetailsResponseSchema.safeParse(json);
+    if (!parseResult.success) {
+        console.error(parseResult.error);
+        return thunkAPI.rejectWithValue("Invalid response format");
+    }
+
+    return mapperToPlace(parseResult.data);
+});
+
+interface CachedPlace {
+    data: Place;
+    timestamp: number;
+}
 
 interface PlaceState {
     data: Place | null;
     loading: boolean;
     error: string | null;
+    cache: Record<string, CachedPlace>;
 }
 
 const initialState: PlaceState = {
     data: null,
     loading: false,
     error: null,
+    cache: {},
 };
 
 const placeSlice = createSlice({
@@ -46,6 +63,12 @@ const placeSlice = createSlice({
             .addCase(fetchPlaceById.fulfilled, (state, action) => {
                 state.loading = false;
                 state.data = action.payload;
+
+                const id = action.payload.id;
+                state.cache[id] = {
+                    data: action.payload,
+                    timestamp: Date.now(),
+                };
             })
             .addCase(fetchPlaceById.rejected, (state, action) => {
                 state.loading = false;
@@ -59,6 +82,7 @@ const placeSlice = createSlice({
 
 function mapperToPlace(place: PlaceDetailsResponse): Place {
     return {
+        id: place.id,
         name: place.name,
         address: place.formattedAddress,
         category: place.primaryTypeDisplayName?.text || null,
